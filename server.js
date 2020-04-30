3125683468d384194ffc624ee6df118e773ae97e
Cezara <cezaraluca@yahoo.com>
2020-04-30 17:21:34 +0300

    get data from api

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
+const cors = require("cors");
 
 const app = express();
+
+//allow cross-origin
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -10,7 +14,7 @@ app.use(
     schema,
     graphiql: true,
   })
-);
+); //endpoint
 
 const PORT = process.env.PORT || 5000;
 
